af87e057b942d490f1214cbc961606b4db130836
mido <midohany910@gmail.com>
2023-06-11 12:39:08 +0300

    GUI of CameraScreen - Implement record(), stop()

--- App-4-Webcam-Photo-Sharer_2/main.py
+++ App-4-Webcam-Photo-Sharer_2/main.py
@@ -1,19 +1,28 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.window import Window
 
-from filesharer import FileSharer
+# from filesharer import FileSharer
 
 Builder.load_file("frontend.kv")
 
 class VidScreen(Screen):
+
     def record(self):
-        pass
+        self.ids.camera.play = True
+        print(self.ids.record_btn.background_color)
+        self.ids.record_btn.background_color = (100, 0, 0, 1)
+        self.ids.record_btn.text = "Stop"
+        
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.record_btn.background_color = (1, 1, 1, 1)
+        self.ids.camera.texture = None
+        self.ids.record_btn.text = "Start Recording"
+
     def capture(self):
         pass
-    pass
 
 class CamScreen(Screen):
     pass
@@ -23,6 +32,7 @@ class RootWidget(ScreenManager):
 
 class MainApp(App):
     def build(self):
+        Window.size = (Window.width * 0.7, Window.height * 0.8)
         return RootWidget()
 
-MainApp.run()
+MainApp().run()
