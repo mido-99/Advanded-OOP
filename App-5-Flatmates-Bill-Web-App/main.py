b454f9cdbcc286c3cfb46f2226ea82b62228fd35
mido <midohany910@gmail.com>
2023-06-14 14:51:59 +0300

    Classes defining

---
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask.views import MethodView
+from wtforms import Form
+
+app = Flask(__name__)
+
+class HomePage(MethodView):
+    
+    def get(self):
+        return "Hello! Home Page"
+
+class BillPage(MethodView):
+    def get(self):
+        return "Bill Page "
+
+class ResultPage(MethodView):
+    pass
+
+class BillForm(Form):
+    pass
+
+app.add_url_rule('/', view_func=HomePage.as_view("home_page"))
+app.add_url_rule('/bill', view_func=BillPage.as_view("bill_page"))
+
+
+app.run(debug=True)
+
+"""
+Basically; Flask is based on functions (using the @app.route('/<route>')). Though we can work by OOP in which each class represents a webpage in our app.
+So to tell Flask to treat this class as a func we write app.add_url_rule()
+
+"""
