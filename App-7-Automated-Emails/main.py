bd01b2c5f3309a5b29278baaf7ee372b597feee3
mido <midohany910@gmail.com>
2023-06-21 00:09:37 +0300

    Constructing email body with titles and urls for news

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -8,17 +8,34 @@ api = env["api_news"]
 
 
 class NewsFeed:
+    '''Generate Recent News Feed for personal interests and send them as an email'''
     
-    def __init__(self, data):
-        self.data = data
-        
-    def get(**kwargs):
-        topic = 'tesla'
-        from_date = '2023-06-15'
-        to_date = '2023-06-20'
-        
-        res = requests.get(f"https://newsapi.org/v2/everything?qintitle={topic}\
-                            &from={from_date}&to={to_date}&language=en&apiKey={api}")
-        
-        pprint(res.text)
+    base_url = "https://newsapi.org/v2/everything?"
+    
+    def __init__(self, topic="NASA", from_date='2023-06-15', to_date='2023-06-20', lang='en'):
+        self.topic = topic
+        self.from_date = from_date
+        self.to_date = to_date
+        self.lang = lang
 
+    def get(self, **kwargs):
+        # Get the response content in json format
+        res = requests.get(f"{self.base_url}qInTitle={self.topic}"\
+                            f"&from={self.from_date}&to={self.to_date}"\
+                            f"&Language={self.lang}&apiKey={api}")
+
+        articles = res.json()['articles']
+        
+        # Extract desired data & create the email body
+        email_body = ''
+        
+        for article in articles:
+            title = article['title']
+            url = article['url']
+            
+            email_body += f"{title}\n{url}\n\n"
+        
+        return email_body
+
+news = NewsFeed().get()
+print(news)
