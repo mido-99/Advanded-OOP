c7930c080e58be01170a651e8606dd32cf0b4330
mido <midohany910@gmail.com>
2023-06-21 23:43:38 +0300

    Date based news & Excuting in a certain hour

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -2,6 +2,7 @@ import yagmail
 from dotenv import dotenv_values
 import pandas
 from news_feed import NewsFeed
+import datetime, time
 
 
 # Create a sender email instance
@@ -11,34 +12,29 @@ email = yagmail.SMTP("momhahah9@gmail.com", password=password)
 # Get email addresses from database
 df = pandas.read_excel("people.xlsx")
 
-
-for index, row in df.iterrows():
-    
-    # Create the news feed
-    news = NewsFeed(topic=row['Interest']).get()
-    
-    # Send email to each person with their interest
-    email.send(to=row["Email"], 
-            subject=f"Today's News about {row['Interest']}!", 
-            contents=f"Hi {row['Name']}\n"\
-                    f"These are some news about {row['Interest']} we've collected for you.\n\n"\
-                    f"{news}")
+# Today's date
+today = datetime.datetime.now().date()
+yesterday = (today - datetime.timedelta(1))
+today = str(today)
+yesterday = str(yesterday)
 
 
+while True:
+        
+    # Runnig the loop in a certain time
+    if datetime.datetime.now().hour == 10 and datetime.datetime.now().minute == 0:
+        time.sleep(60)
+        
+        for index, row in df.iterrows():
+        
+            # Create the news feed
+            news = NewsFeed(topic=row['Interest'], from_date=yesterday, to_date=today).get()
+            
+            # Send email to each person with their interest
+            email.send(to=row["Email"], 
+                    subject=f"Today's News about {row['Interest']}!", 
+                    contents=f"Hi {row['Name']}\n"\
+                            f"These are some news about {row['Interest']} we've collected for you:\n\n"\
+                            f"{news}")
 
-
-
-
-
-
-
-
-
-
-# df = pandas.DataFrame([["Name", "Email", "Interest"],
-#                         ["Soso", "lihevo4293@anwarb.com", "Space"],
-#                         ["Lolo", "xzavier.edel@donebyngle.com", 'Football'],
-#                         ["Toto", "pawefibe@afia.pro", "Maths"]])
-
-# df.to_excel("newpeople.xlsx", header=False, index=False)
 
