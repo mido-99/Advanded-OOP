aaa318a02528f1610c95102332b8f94d7b392ab4
mido <midohany910@gmail.com>
2023-07-06 23:09:44 +0300

    Snippet code: send email to one address once

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -18,7 +18,7 @@ yesterday = (today - datetime.timedelta(1))
 today = str(today)
 yesterday = str(yesterday)
 
-
+## Multiple emails ##
 while True:
         
     # Runnig the loop in a certain time
@@ -37,4 +37,15 @@ while True:
                             f"These are some news about {row['Interest']} we've collected for you:\n\n"\
                             f"{news}")
 
+## For one email ##
+# for index, row in df.iterrows():
 
+#     # Create the news feed
+#     news = NewsFeed(topic=row['Interest'], from_date=yesterday, to_date=today).get()
+    
+#     # Send email to each person with their interest
+#     email.send(to=row["Email"], 
+#             subject=f"Today's News about {row['Interest']}!", 
+#             contents=f"Hi {row['Name']}\n"\
+#                     f"These are some news about {row['Interest']} we've collected for you:\n\n"\
+#                     f"{news}")
