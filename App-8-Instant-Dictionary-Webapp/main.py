525b20cd5c20247af443646bebe71b27bcc53336
mido <midohany910@gmail.com>
2023-07-15 00:37:30 +0300

    class & static methods used in webapp

--- App-8-Instant-Dictionary-Webapp/main.py
+++ App-8-Instant-Dictionary-Webapp/main.py
@@ -7,3 +7,22 @@ jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
 
 jp.justpy(port=8001)
+
+"""
+When JustPy executes the passed serve method it doesn't intanimate an instance of this object;
+it simply calls it as a #* class method: About.serve()
+And as doing this will cause an error -since you can't call an inst method from a class you have
+to create an instance of it- So justpy passes a request object to the serve().
+- This way the request will be treated as self. And hence we reach the final poing that self
+is not actually referring to our class! But to the passed request in this case.
+- Which will give us just problems in case we need to refer to self.anything, thinking that we're
+working with the class.
+- The key to solve this issue is by using #* @classmethod
+As mentioned above; we're calling the method from the class itself not an instance (inst is just
+created when someone visits the webpage).
+
+- Thought until now we've worked out half of the problem only, we still need that request object
+in case we want to refer to it or its attrs, and to solve this it's really simple; just pass any
+arbitrary var as second param for serve(cls, req) say req, and that's it!
+
+"""
--- App-8-Instant-Dictionary-Webapp/webapp/home.py
+++ App-8-Instant-Dictionary-Webapp/webapp/home.py
@@ -3,11 +3,40 @@ import justpy as jp
 class Home():
     path = '/'
     
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        dad = jp.Div(a= wp, classes='bg-gray-200 h-screen text-lg p-8')
+        dad = jp.Div(a= wp, classes='bg-gray-200 h-screen text-xl p-8')
         
-        div1 = jp.Div(a= dad, text='This is a       Home page', classes='text-3xl')
-        div2 = jp.Div(a= dad, text='This is bigger div ', classes='text-6xl')
+        jp.Div(a= dad, text='Instant Dictionary', classes='text-4xl font-italic')
+        jp.Div(a= dad, text='Type any word or phrase ', classes='')
         
+        inp_div = jp.Div(a= dad, classes='grid grid-cols-2 gap-4 m-8')
+        term = jp.Input(a=inp_div, placeholder='Type here...', classes='border-4 rounded-md'
+                        ' bg-gray-100 focus:bg-white focus:border-purple-600 focus:outline-none'
+                        ' border-purple-400')
+        definition = jp.Div(a=dad, text='Definition here', classes='border-8 border-purple-400 h-40'
+                        ' border-double')
+        btn = jp.Button(a=inp_div, text="what does this mean?", classes='border-4 border-purple-400'
+                        ' rounded-md outline-none', 
+                        click=cls.get_def, term=term, definition=definition)
         return wp
+    
+    @staticmethod
+    def get_def(widget, msg):
+        widget.definition.text = 'Boom'
+
+
+"""
+#* REMEMBER that if we use self like this:
+    def get_def(self):
+As mentioned in main file; We're not working with class instances, with the class itself instead.
+So if we consider using @classmethod; We are pushed then to use cls like this:
+    @classmethod
+    def get_def(cls):
+Thus we can't use widget, msg which is our main goal.
+
+* The solution in such case is To use @staticmethod simply. Since they don't require args like
+self and cls, so they could be used inside a class to do a specific function without either 
+refering to class like @classmethod, or instantiating an instance of it like @classmethod
+"""
